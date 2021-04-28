1cc84f73842f59babae37f336eb5c5f464337b4b
ykrsama <zhuxuliang@outlook.com>
2021-04-28 15:41:09 +0800

    update lib name

--- loadDLL.sh
+++ loadDLL.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-export MARLIN_DLL=/workfs2/cepc/xuliang/processor/ana4b/lib/libSimpleAna.so:$MARLIN_DLL
+export MARLIN_DLL=/workfs2/cepc/xuliang/processor/ana4b/lib/libAna4b.so:$MARLIN_DLL
