fb9a2dea0adbdc8179f4774aeaac5d846017a91b
ykrsama <zhuxuliang@outlook.com>
2021-05-12 11:37:56 +0800

    add RecSinglet

---
+++ include/RecSinglet.hh
@@ -0,0 +1,21 @@
+#ifndef _RecSinglet_hh_
+#define _RecSinglet_hh_
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "marlin/Processor.h"
+#include "LCIOSTLTypes.h"
+
+#include "TTree.h"
+#include "TFile.h"
+
+
+class TTree;
+
+
+class Singlet : public marlin::Pr
+
+#endif
